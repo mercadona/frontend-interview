345afe653f72431eb2e650dacf5f2881a5b5fae0
Juan Garnica <juan@harbiz.io>
2025-10-04 09:15:39 +0200

    feat: enhance Home component to utilize cartData for displaying current product quantities in ProductCard

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,22 +6,29 @@ import { ProductCard } from 'system-ui/ProductCard'
 import './Home.styles.css'
 
 export const Home = () => {
-  const { updateCart: setCartData, productsData: products } =
-    useContext(CartContext)
+  const {
+    updateCart: setCartData,
+    productsData: products,
+    cartData,
+  } = useContext(CartContext)
 
   return (
     <section className="Home-ProductList" data-testid="Home-ProductList">
-      {products.map(({ id, price, name, image_url, description }) => (
-        <ProductCard
-          key={id}
-          price={price}
-          id={id}
-          name={name}
-          image_url={image_url}
-          description={description}
-          setCartData={setCartData}
-        />
-      ))}
+      {products.map(({ id, price, name, image_url, description }) => {
+        let currentQuantity = cartData[id]
+        return (
+          <ProductCard
+            key={id}
+            price={price}
+            id={id}
+            name={name}
+            image_url={image_url}
+            description={description}
+            setCartData={setCartData}
+            currentQuantity={currentQuantity}
+          />
+        )
+      })}
     </section>
   )
 }
